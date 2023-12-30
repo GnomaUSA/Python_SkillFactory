35fc3e6c3cd556a105f3186699bb5e234cb6b031
Gnoma <138181983+GnomaUSA@users.noreply.github.com>
2023-12-30 15:08:44 -0500

    Add files via upload
    Добавлены комментарии

--- Algorithmic task_1.py
+++ Algorithmic task_1.py
@@ -11,11 +11,11 @@ def find_median(arr):
     if n == 0:
         return None
     elif n % 2 == 0:
-        median = (arr[n//2] + arr[n//2 - 1]) / 2
+        median = (arr[n//2] + arr[n//2 - 1]) / 2 # деление с дробной частью
         return median
     else:
         n % 2 ==1
-        median = arr[n//2]
+        median = arr[n//2] # цельночисленное деление, дробная часть отбрасывается
         return median
 
 print(arr)
