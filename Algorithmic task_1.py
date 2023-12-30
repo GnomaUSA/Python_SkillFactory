68ce6bdd0153b9a60ac644c08623572522d4ce46
Gnoma <138181983+GnomaUSA@users.noreply.github.com>
2023-12-30 15:05:42 -0500

    Add files via upload

---
+++ Algorithmic task_1.py
@@ -0,0 +1,24 @@
+# Напишите функцию find_median, которая будет возвращать
+# одно число - медианное значение.
+# Если переданный в функцию список окажется пустым - верните None.
+# Функция find_median принимает на вход
+# arr - исходный список с числами.
+
+arr = [100, 5, 2, 4, 3, 6]
+arr.sort()
+def find_median(arr):
+    n = len(arr)
+    if n == 0:
+        return None
+    elif n % 2 == 0:
+        median = (arr[n//2] + arr[n//2 - 1]) / 2
+        return median
+    else:
+        n % 2 ==1
+        median = arr[n//2]
+        return median
+
+print(arr)
+print(find_median(arr))
+
+
